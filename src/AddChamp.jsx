a18c8f42cea17efc195f1c65d390eaf778c8c57b
Kevin Van Humbeeck <kevin.vanhumbeeck@mediahuis.be>
2022-01-07 12:20:14 +0100

    add champ

---
+++ src/AddChamp.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+const AddChamp = ({ onAdd }) => {
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [flag, setFlag] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      alert("Please add a task");
+      return;
+    }
+
+    onAdd({ name, type, flag });
+
+    setName("");
+    setType("");
+    setFlag(false);
+  };
+
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <label>
+          Name
+          <input
+            type="text"
+            placeholder="Add Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="form-control">
+        <label>
+          Type
+          <input
+            type="text"
+            placeholder="Add Type"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="form-control form-control-check">
+        <label>
+          Active
+          <input
+            type="checkbox"
+            checked={flag}
+            value={flag}
+            onChange={(e) => setFlag(e.currentTarget.checked)}
+          />
+        </label>
+      </div>
+
+      <input type="submit" value="Save Task" className="btn btn-block" />
+    </form>
+  );
+};
+
+export default AddChamp;
